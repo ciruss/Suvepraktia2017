117d2fa8a36b5166868d7c9f1c3fc0da1beca1b5
Mattias Blehner <matttias@hithub.com>
2017-06-09 14:35:56 +0300

    Input-i kontroll

--- calculator.js
+++ calculator.js
@@ -7,11 +7,13 @@ var m1x, m1y, m2x, m2y;
 
 // **** ÜLDINE FUNKTSIOON MAATRIKSITE GENEREERIMISEKS ****
 function generateMatrix() {
-    //et kastid jälle nähtavale ilmuks
-    document.getElementById("checkAnswer").style.display = "block";
+   
+   //et kastid jälle nähtavale ilmuks
+    document.getElementById("checkAnswer").style.display = "block";	
     document.getElementById("calculateNext").style.display = "block";
     document.getElementById("matrix1Container").style.display = "inline";
     document.getElementById("matrix2Container").style.display = "inline";
+	document.getElementById("negativeNumber").style.display = "block";
 
 	m1x = document.getElementById("m1x").value;
 	m1y = document.getElementById("m1y").value;
@@ -76,6 +78,10 @@ function createMatrix1() {
 			var cell = document.createElement("input");
 			cell.setAttribute("id", "a" + rowId + colId);
 			cell.setAttribute("type", "text");
+			cell.setAttribute("onkeypress", "validate(event)");
+			//cell.setAttribute("pattern", "\d");
+			//cell.setAttribute("onkeypress", "return validate(this, event)");//<--TÖÖTAB
+			cell.setAttribute("maxlength", "10");
 			row.appendChild(cell);
 		}
 		tableBody.appendChild(row);
@@ -109,6 +115,9 @@ function createMatrix2() {
 			var cell = document.createElement("input");
 			cell.setAttribute("id", "b" + rowId + colId);
 			cell.setAttribute("type", "text");
+			cell.setAttribute("onkeypress", "validate(event)");
+			//cell.setAttribute("onkeypress", "return validate(this, event)");//<--TÖÖTAB
+			cell.setAttribute("maxlength", "10");
 			row.appendChild(cell);
 		}
 		tableBody.appendChild(row);
@@ -205,9 +214,6 @@ function calculateNextMatrix() {
     document.getElementById("matrix2Container").style.display = "none";
     document.getElementById("checkAnswer").style.display = "none";
     document.getElementById("calculateNext").style.display = "none";
-
-
-
 }
 
 // **** GENEREERIB VAHETULEMUSE ****
@@ -256,6 +262,67 @@ function calculateMatrixFinalAnswer() {
 			var strLength = matrixAnswerString.length;
 			matrixAnswer.value = math.eval(matrixAnswerString.slice(0, strLength - 3));
 			c = 1;
+		}
+	}
+}
+
+// **** LASEB SISESTADA AINULT NUMBREID JA ÜHE KALDKRIIPSU, ET SAAKS SISESTADA MURDE ****
+/*
+function validate(elementRef, event) {
+	var keyCodeEntered = (event.which) ? event.which : (window.event.keyCode) ? window.event.keyCode : -1;
+		console.log(event.keyCode);
+		if ((keyCodeEntered >= 48) && (keyCodeEntered <= 57) || (keyCodeEntered === 8)) {
+			return true;
+		} else if (keyCodeEntered == 47) {
+			if ((elementRef.value) && (elementRef.value.indexOf('/') >= 0)) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+	return false;
+}
+*/
+
+
+function validate(evt) {
+	var theEvent = evt || window.event;
+	var key = theEvent.keyCode || theEvent.which;
+	key = String.fromCharCode( key );
+	var regex = /^[0-9]*\/?[0-9]*$/;
+	//([0-9]+(\.[0-9]+)?)
+	//var regex = /\d+\.\d+/;
+	
+	console.log(evt.target);
+	if(key==="/" && evt.target.value.indexOf('/') != -1){
+		theEvent.returnValue = false;
+			if(theEvent.preventDefault) theEvent.preventDefault();
+	}
+	
+	if( !regex.test(key) ) {
+		theEvent.returnValue = false;
+			if(theEvent.preventDefault) theEvent.preventDefault();
+	}
+	
+}
+
+function negativeNumber(){
+	for (var i = 0; i < m1x; i++) {
+		for (var j = 0; j < m1y; j++) {
+			var rowId = i + 1;
+			var colId = j + 1;
+			var boxID = ("a" + rowId + colId);
+			console.log(boxID);
+			var number = document.getElementById("a" + rowId + colId).value;
+			parseInt(number);
+			console.log("Sisend: "+number);
+			
+			if(number<0){
+				console.log("Number on väiksem");
+			} else {
+				console.log("Number on suurem");
+			}		
+			
 		}
 	}
 }
