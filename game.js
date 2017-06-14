b9f6b384efbd21a1c6092f9b03301852dcb6ee07
Mattias Blehner <matttias@hithub.com>
2017-06-14 13:30:31 +0300

    Lisasin uued menüü nuppud

--- game.js
+++ game.js
@@ -22,6 +22,8 @@ var Em1x, Em1y, Em2x, Em2y;
 var a = [[null, null, null]];
 var b = [[null, null, null]];
 
+var matrixCellValue;
+var matrixCellValuePreAnswers;
 // ||||| ----- ----- ----- ----- MAATRIKSITE HARJUTAMISE OSA ----- ----- ----- ----- |||||
 
 //mängija nime küsimine
@@ -235,8 +237,8 @@ function createExerciseMatrixPreAnswer() {
 			cell.setAttribute("id", "Ed" + rowId + colId);
 			cell.setAttribute("class", "matrixAnswerInput");
 			cell.setAttribute("type", "text");
-			cell.setAttribute("onkeypress", "return validate(this, event)");
-			//cell.setAttribute("oninput", "checkLength(2,this)");
+			cell.setAttribute("onkeypress", "validate(event)");
+			//cell.setAttribute("onblur", "checkInputSequence()");
 			row.appendChild(cell);
 		}
 		tableBody.appendChild(row);
@@ -270,7 +272,8 @@ function createExerciseMatrixAnswer() {
 			var cell = document.createElement("input");
 			cell.setAttribute("id", "Ec" + rowId + colId);
 			cell.setAttribute("type", "text");
-			cell.setAttribute("onkeypress", "return validate(this, event)");
+			cell.setAttribute("onkeypress", "validate(event)");
+			//cell.setAttribute("onblur", "checkInputSequence()");
 			row.appendChild(cell);
 		}
 		tableBody.appendChild(row);
@@ -334,13 +337,17 @@ function checkMatrixFinalAnswers() {
 
 			for (var i = 0; i < Em1y; i++) {
 				var Ea = document.getElementById("Ea" + rowId + c).value;
+				//console.log("checkMatrixFinalAnswers Ea: "+Ea);
 				var Eb = document.getElementById("Eb" + c + colId).value;
+				//console.log("checkMatrixFinalAnswers Eb: "+Eb);
 				matrixAnswerString += Ea + "*" + Eb + " + ";
 				c++;
 			}
 			var strLength = matrixAnswerString.length;
-			var matrixCellValue = math.eval(matrixAnswerString.slice(0, strLength - 3));
+			matrixCellValue = math.eval(matrixAnswerString.slice(0, strLength - 3));
+			console.log("Kalkulaatori lõppvastus matrixCellValue: "+matrixCellValue);
 			var matrixInputCell = parseInt(matrixAnswer.value);
+			console.log("Kasutaja lõppvastus matrixInputCell: "+matrixInputCell);
 			c = 1;
 
 			if (matrixInputCell == matrixCellValue) {
@@ -376,12 +383,20 @@ function checkMatrixPreAnswers() {
 				matrixAnswerString += Ea + "*" + Eb + " + ";
 				c++;
 			}
+			
 			var strLength = matrixAnswerString.length;
-			var matrixCellValue = matrixAnswerString.slice(0, strLength - 3);
-			var matrixInputCell = matrixAnswer.value;
+			matrixCellValuePreAnswers = matrixAnswerString.slice(0, strLength - 3);
+			console.log("matrixCellValuePreAnswers: "+matrixCellValuePreAnswers);
+			
+			var test = eval(matrixCellValuePreAnswers);
+			console.log("test: " +test);
+			
+			//console.log("matrixCellValue: "+matrixCellValue);
+			var matrixInputCell = eval(matrixAnswer.value);
+			//console.log("matrixInputCell: "+matrixInputCell);
 			c = 1;
 
-			if (matrixInputCell === matrixCellValue) {
+			if (matrixInputCell === test) {
 				matrixAnswer.style.color = "green";
 				//answerCounter += 1
 				//score += 1
@@ -410,13 +425,17 @@ function justForDevsMatrixAnswers() {
 			for (var i = 0; i < Em1y; i++) {
 
 				var Ea = document.getElementById("Ea" + rowId + c).value;
+				//console.log("justForDevsMatrixAnswers Ea: "+Ea);
 				var Eb = document.getElementById("Eb" + c + colId).value;
+				//console.log("justForDevsMatrixAnswers Eb: "+Eb);
 				matrixAnswerString += Ea + "*" + Eb + " + ";
 				c++;
 			}
 			var strLength = matrixAnswerString.length;
-			matrixAnswer.value = matrixAnswerString.slice(0, strLength - 3);
+			math.eval(matrixAnswer.value = matrixAnswerString.slice(0, strLength - 3));
+			//console.log("justForDevsMatrixAnswers matrixAnswer.value: "+matrixAnswer.value);
 			matrixFinalAnswer.value = math.eval(matrixAnswerString.slice(0, strLength - 3));
+			//console.log("justForDevsMatrixAnswers matrixFinalAnswer.value: "+matrixFinalAnswer.value);
 			c = 1;
 		}
 	}
@@ -469,8 +488,8 @@ function checkMatrixAnswersRandom() {
 	matrixPreAnswerErrors = 0;
 }
 
+/*
 //TULEMUSTE SALVESTAMINE LOCALSTORAGESSE
-
 function saveStatsToLocalstorage(dataObject) {
 	if (!localStorage["top"]) {
 		var arr = []
@@ -498,6 +517,7 @@ function loadStatsFromLocalStorage() {
 			createTable(arr)
 	}
 }
+*/
 
 // MÄNGU SKOORI FUNKTSIOONID
 function resetScore() {
@@ -528,7 +548,6 @@ function updateScore() {
 
 //UUE MÄNGU ALUSTAMINE
 function startNewGame() {
-
 	document.getElementById("generateExerciseMatrix").style.visibility = "visible";
 	document.getElementById("generateRandomExerciseMatrix").style.visibility = "visible";
 	document.getElementById("startNewGame").style.visibility = "hidden";
@@ -562,6 +581,8 @@ function tick() {
 		clearInterval(ticker)
 	}
 }
+
+
 // **** KONTROLLIB SISESTUST *****
 function validate(evt) {
 	key = evt.key;
